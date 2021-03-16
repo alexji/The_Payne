c58eed546184a2b3606ac592dc2a4e083d58fc1e
Alexander Ji <aji@carnegiescience.edu>
2021-03-16 09:51:04 -0700

    Set up separate training function to do batches

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -114,9 +114,206 @@ class Payne_model(torch.nn.Module):
 #===================================================================================================
 # train neural networks
 def neural_net(training_labels, training_spectra, validation_labels, validation_spectra,\
-               num_neurons = 300, num_steps=1e4, learning_rate=1e-4, batch_size=512,\
-               num_features = 64*5, mask_size=11, num_pixel=7214, 
-               num_workers=0, training_batch_size=None, validation_batch_size=None):
+                         num_neurons = 300, num_steps=1e4, learning_rate=1e-4, batch_size=512,\
+                         num_features = 64*5, mask_size=11, num_pixel=7214):
+    '''
+    Training a neural net to emulate spectral models
+
+    training_labels has the dimension of [# training spectra, # stellar labels]
+    training_spectra has the dimension of [# training spectra, # wavelength pixels]
+
+    The validation set is used to independently evaluate how well the neural net
+    is emulating the spectra. If the neural network overfits the spectral variation, while
+    the loss will continue to improve for the training set, but the validation
+    set should show a worsen loss.
+
+    The training is designed in a way that it always returns the best neural net
+    before the network starts to overfit (gauged by the validation set).
+
+    num_steps = how many steps to train until convergence.
+    1e4 is good for the specific NN architecture and learning I used by default.
+    Bigger networks will take more steps to converge, and decreasing the learning rate
+    will also change this. You can get a sense of how many steps are needed for a new
+    NN architecture by plotting the loss evaluated on both the training set and
+    a validation set as a function of step number. It should plateau once the NN
+    has converged.
+
+    learning_rate = step size to take for gradient descent
+    This is also tunable, but 1e-4 seems to work well for most use cases. Again,
+    diagnose with a validation set if you change this.
+
+    num_features is the number of features before the deconvolutional layers; it only
+    applies if ResNet is used. For the simple multi-layer perceptron model, this parameter
+    is not used. We truncate the predicted model if the output number of pixels is
+    larger than what is needed. In the current default model, the output is ~8500 pixels
+    in the case where the number of pixels is > 8500, increase the number of features, and
+    tweak the ResNet model accordingly
+
+    batch_size = the batch size for training the neural networks during the stochastic
+    gradient descent. A larger batch_size reduces stochasticity, but it might also
+    risk of stucking in local minima.
+
+    training_batch_size, validation_batch_size: this decides how big of a chunk of data
+    is loaded onto the GPU at once. By default is the same value as batch_size
+    '''
+
+    # run on cuda
+    dtype = torch.cuda.FloatTensor
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    # scale the labels, optimizing neural networks is easier if the labels are more normalized
+    x_max = np.max(training_labels, axis = 0)
+    x_min = np.min(training_labels, axis = 0)
+    x = (training_labels - x_min)/(x_max - x_min) - 0.5
+    x_valid = (validation_labels-x_min)/(x_max-x_min) - 0.5
+
+    # dimension of the input
+    dim_in = x.shape[1]
+
+#--------------------------------------------------------------------------------------------
+    # assume L1 loss
+    loss_fn = torch.nn.L1Loss(reduction = 'mean')
+
+    # make pytorch variables
+    x = Variable(torch.from_numpy(x)).type(dtype)
+    y = Variable(torch.from_numpy(training_spectra), requires_grad=False).type(dtype)
+    x_valid = Variable(torch.from_numpy(x_valid)).type(dtype)
+    y_valid = Variable(torch.from_numpy(validation_spectra), requires_grad=False).type(dtype)
+    
+    # initiate Payne and optimizer
+    model = Payne_model(dim_in, num_neurons, num_features, mask_size, num_pixel)
+    model.cuda()
+    model.train()
+    
+    # we adopt rectified Adam for the optimization
+    optimizer = radam.RAdam([p for p in model.parameters() if p.requires_grad==True], lr=learning_rate)
+
+#--------------------------------------------------------------------------------------------
+    # train in batches
+    nsamples = x.shape[0]
+    nbatches = nsamples // batch_size
+
+    nsamples_valid = x_valid.shape[0]
+    nbatches_valid = nsamples_valid // validation_batch_size
+
+    # initiate counter
+    current_loss = np.inf
+    training_loss =[]
+    validation_loss = []
+
+#-------------------------------------------------------------------------------------------------------
+    # train the network
+    start = time.time()
+    for e in range(int(num_steps)):
+
+        # randomly permute the data
+        perm = torch.randperm(nsamples)
+        perm = perm.cuda()
+        
+        # for each batch, calculate the gradient with respect to the loss
+        for i in range(nbatches):
+            idx = perm[i * batch_size : (i+1) * batch_size]
+            y_pred = model(x[idx])
+        
+            loss = loss_fn(y_pred, y[idx])*1e4
+            optimizer.zero_grad()
+            loss.backward(retain_graph=False)
+            optimizer.step()
+
+#-------------------------------------------------------------------------------------------------------
+        # evaluate validation loss
+        if e % 100 == 0:
+            start2 = time.time()
+
+            # here we also break into batches because when training ResNet
+            # evaluating the whole validation set could go beyond the GPU memory
+            # if needed, this part can be simplified to reduce overhead
+            perm_valid = torch.randperm(nsamples_valid)
+            perm_valid = perm_valid.cuda()
+            loss_valid = 0
+
+            for j in range(nbatches_valid):
+                idx = perm_valid[j * batch_size : (j+1) * batch_size]
+                y_pred_valid = model(x_valid[idx])
+                loss_valid += loss_fn(y_pred_valid, y_valid[idx])*1e4
+            loss_valid /= nbatches_valid
+            
+            print('iter %s:' % e, 'training loss = %.3f' % loss,\
+                  'validation loss = %.3f' % loss_valid,\
+                  'cum time = %.1f' % (time.time()-start), \
+                  'val time = %.1f' % (time.time()-start2))
+            sys.stdout.flush()
+
+            loss_data = loss.detach().data.item()
+            loss_valid_data = loss_valid.detach().data.item()
+            training_loss.append(loss_data)
+            validation_loss.append(loss_valid_data)
+
+#--------------------------------------------------------------------------------------------
+            # record the weights and biases if the validation loss improves
+            if loss_valid_data < current_loss:
+                current_loss = loss_valid_data
+                model_numpy = []
+                for param in model.parameters():
+                    model_numpy.append(param.data.cpu().numpy())
+
+                # extract the weights and biases
+                w_array_0 = model_numpy[0]
+                b_array_0 = model_numpy[1]
+                w_array_1 = model_numpy[2]
+                b_array_1 = model_numpy[3]
+                w_array_2 = model_numpy[4]
+                b_array_2 = model_numpy[5]
+
+                # save parameters and remember how we scaled the labels
+                np.savez("NN_normalized_spectra.npz",\
+                        w_array_0 = w_array_0,\
+                        w_array_1 = w_array_1,\
+                        w_array_2 = w_array_2,\
+                        b_array_0 = b_array_0,\
+                        b_array_1 = b_array_1,\
+                        b_array_2 = b_array_2,\
+                        x_max=x_max,\
+                        x_min=x_min,)
+
+                # save the training loss
+                np.savez("training_loss.npz",\
+                         training_loss = training_loss,\
+                         validation_loss = validation_loss)
+
+#--------------------------------------------------------------------------------------------
+    # extract the weights and biases
+    w_array_0 = model_numpy[0]
+    b_array_0 = model_numpy[1]
+    w_array_1 = model_numpy[2]
+    b_array_1 = model_numpy[3]
+    w_array_2 = model_numpy[4]
+    b_array_2 = model_numpy[5]
+
+    # save parameters and remember how we scaled the labels
+    np.savez("NN_normalized_spectra.npz",\
+             w_array_0 = w_array_0,\
+             w_array_1 = w_array_1,\
+             w_array_2 = w_array_2,\
+             b_array_0 = b_array_0,\
+             b_array_1 = b_array_1,\
+             b_array_2 = b_array_2,\
+             x_max=x_max,\
+             x_min=x_min,)
+
+    # save the final training loss
+    np.savez("training_loss.npz",\
+             training_loss = training_loss,\
+             validation_loss = validation_loss)
+
+    return
+
+#===================================================================================================
+# train neural networks on large datasets
+def neural_net_loadbatch(training_labels, training_spectra, validation_labels, validation_spectra,\
+                         num_neurons = 300, num_steps=1e4, learning_rate=1e-4, batch_size=512,\
+                         num_features = 64*5, mask_size=11, num_pixel=7214, 
+                         num_workers=0, training_batch_size=None, validation_batch_size=None):
     '''
     Training a neural net to emulate spectral models
 
